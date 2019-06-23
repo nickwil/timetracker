8f2c7b580f533d051a89a60311e96656a9f87cf0
Nicolas <nickwil@users.noreply.github.com>
2019-06-23 14:32:09 -0400

    moved animated input to its own file

---
+++ src/AnimatedInput.jsx.js
@@ -0,0 +1,35 @@
+import React from "react"
+import {useSpring, animated} from 'react-spring'
+
+function AnimatedInput ({outsideChange, extras = {}}){
+    const [showAnimation, changeAnimation] = useState(false) 
+    const style = useSpring({
+    width: showAnimation ? `25%` : `0%`,
+    from: { width: `0%`, height: `0.25em`, backgroundColor: `blue` },
+  })
+  const [value, onChange] = useState("")
+  const handleOnChange  = (text) => {
+    if(text != ''){
+      changeAnimation(true)
+    } else {
+      changeAnimation(false)
+    }
+    onChange(text)
+    
+    outsideChange(text, extras)
+  }
+  const handleOnBlur = () => {
+    changeAnimation(false)
+  }
+  const handleOnFocus = () => {
+    if(value != ''){
+      changeAnimation(true)
+    }
+  }
+return (<div>
+          <input onFocus={() => handleOnFocus()} style={{width: `25%`, border: `0`}} onBlur={() => handleOnBlur()} onChange={(e) => handleOnChange(e.target.value)} value={value}/>
+          <animated.div style={style}></animated.div>
+      </div>)
+}
+
+export default AnimatedInput
--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {useSpring, animated} from 'react-spring'
 import Timer from "./Timer.jsx"
 import Tags from "./Tags.jsx"
 import Calendar from "./Calendar.jsx"
@@ -89,37 +88,5 @@ const App = observer(function App({store, dayFromUrl}) {
     </div>
   );
 })
-
-function AnimatedInput ({outsideChange, extras = {}}){
-    const [showAnimation, changeAnimation] = useState(false) 
-    const style = useSpring({
-    width: showAnimation ? `25%` : `0%`,
-    from: { width: `0%`, height: `0.25em`, backgroundColor: `blue` },
-  })
-  const [value, onChange] = useState("")
-  const handleOnChange  = (text) => {
-    if(text != ''){
-      changeAnimation(true)
-    } else {
-      changeAnimation(false)
-    }
-    onChange(text)
-    
-    outsideChange(text, extras)
-  }
-  const handleOnBlur = () => {
-    changeAnimation(false)
-  }
-  const handleOnFocus = () => {
-    if(value != ''){
-      changeAnimation(true)
-    }
-  }
-return (<div>
-          <input onFocus={() => handleOnFocus()} style={{width: `25%`, border: `0`}} onBlur={() => handleOnBlur()} onChange={(e) => handleOnChange(e.target.value)} value={value}/>
-          <animated.div style={style}></animated.div>
-      </div>)
-}
-
 
 export default App;
