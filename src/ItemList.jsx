b0ece9af286a3bfb0b6d8ec889bffe5c1b5c525d
Nicolas <nickwil@users.noreply.github.com>
2019-06-23 14:27:08 -0400

    changed name from item and addded render props

--- src/ItemList.jsx
+++ src/ItemList.jsx
@@ -2,14 +2,14 @@ import React from "react"
 import Tags from "./Tags.jsx"
 import Input from "./Input.jsx"
 import ItemTime from "./ItemTime.jsx"
-function ItemList(props){
+function ItemList({store, item, changeIfCounting}){
 	return <div>
-			  <ItemTime id={obj.id} changeIfCounting={changeIfCounting} time={obj.tilCompletion}/>
-              <Input styles={styles.input} text={obj.text} id={obj.id} 
-              onTaskChange={(text) => store.items[store.index(obj.id)].updateText(text)}/>
+			  <ItemTime id={item.id} changeIfCounting={changeIfCounting} time={item.tilCompletion}/>
+              <Input styles={styles.input} text={item.text} id={item.id} 
+              onTaskChange={(text) => store.items[store.index(item.id)].updateText(text)}/>
               <Tags 
-              onChange={(tag) => store.items[store.index(obj.id)].updateTag(tag)} 
-              defaultTagId={obj.tagId} tags={tags}/>
+              onChange={(tag) => store.items[store.index(item.id)].updateTag(tag)} 
+              defaultTagId={item.tagId} tags={tags}/>
            </div>
 }
 export default ItemList
