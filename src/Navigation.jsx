f1c5ccef8a9a23c136b717bbfc630ee147a59611
Nicolas <nickwil@users.noreply.github.com>
2019-06-27 09:57:03 -0400

    added formater to navigation

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -2,10 +2,12 @@ import React from "react"
 import CustomLink from "./CustomLink.jsx"
 import styles from "./Navigation.module.css"
 import store from "./store.js"
+import shortTimeFormatting from "./util/shortTimeFormatting.js"
+
 function Navigation(props){
 	return(
 		<nav className={styles.container}>
-			<CustomLink to="/">Home - {store.getTimeToSpendForDay()}s</CustomLink>
+			<CustomLink to="/">Home - {shortTimeFormatting(store.getTimeToSpendForDay())}</CustomLink>
 			<CustomLink to="calendar/">Calendar</CustomLink>
 			<CustomLink to="settings/">Settings</CustomLink>
 			Stats
