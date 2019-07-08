c70de72fa3d2dfaf9e2e503fec6d2f29334678d9
Nicolas <nickwil@users.noreply.github.com>
2019-07-07 21:12:16 -0400

    created month stats component

---
+++ src/MonthStats.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Stats from "./Stats.jsx"
+
+import store from "./store.js"
+import tagStore from "./tagStore.js"
+
+function MonthStats({year, month}){
+	const items = store.completedTodosFromMonth(year, month)
+	return <Stats data={store.getTimeFromEachTag(tagStore.tags, items)} />
+}
+export default MonthStats
