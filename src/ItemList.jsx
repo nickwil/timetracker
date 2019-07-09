0c12e79aef41a0c319663a8cd02f59c66aca6b5e
Nicolas <nickwil@users.noreply.github.com>
2019-07-09 16:03:24 -0400

    added color symbols to items based on their tag

--- src/ItemList.jsx
+++ src/ItemList.jsx
@@ -4,8 +4,10 @@ import Input from "./Input.jsx"
 import ItemTime from "./ItemTime.jsx"
 import ItemMenuContainer from "./ItemMenu.jsx"
 import { observer } from 'mobx-react-lite'
+import tagStore from "./tagStore.js"
 
 import styles from "./ItemList.module.css"
+
 const ItemList = observer(function ItemList({store, item, tags}){
 	const storeItem = store.items[store.index(item.id)]
 	return <div className={styles.container}>
@@ -13,11 +15,12 @@ const ItemList = observer(function ItemList({store, item, tags}){
 
               <Input styles={styles.input} text={item.text} id={item.id} 
               onTaskChange={(text) => storeItem.updateText(text)}/>
-
-              <Tags 
-              onChange={(tag) => storeItem.updateTag(tag)} 
-              defaultTagId={item.tagId} tags={tags}/>
-
+              <span>
+                <span style={{color: tagStore.tags[tagStore.index(item.tagId)].color}}>•</span>
+                <Tags 
+                onChange={(tag) => storeItem.updateTag(tag)} 
+                defaultTagId={item.tagId} tags={tags}/>
+              </span>
               <ItemMenuContainer
               store={store}
               id={item.id}/>
