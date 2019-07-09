5efb40a8715f2ac5dfac19dc43c8edd1ca57c9a3
Nicolas <nickwil@users.noreply.github.com>
2019-07-09 19:44:05 -0400

    added links to the bottom of the stats page with all the filter options

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -3,9 +3,11 @@ import PieChart from 'react-minimal-pie-chart';
 import store from "./store.js"
 import shortTimeFormatting from "./util/shortTimeFormatting.js"
 import { observer } from 'mobx-react-lite'
+import CustomLink from "./CustomLink.jsx"
 
 const colors = ["#E38627", '#C13C37', '#6A2135']
 const Stats = observer(function Stats({data}){
+  console.log(store.years)
 	return (<div>
 		<h4>Time Spent Working</h4>
 		<PieChart 
@@ -28,6 +30,29 @@ const Stats = observer(function Stats({data}){
 	{
 		data.map((obj)=> <div>{obj.title} - <span style={{color: obj.color}}>•</span></div>)
 	}
+</section>
+
+<section>
+  <h3>See stats throughout your history</h3>
+  <div>
+  <h4>Years</h4>
+  {
+    store.years.map((year) => <div><CustomLink to={"/stats/" + year}>{year}</CustomLink></div>)
+  }
+  </div>
+  <div>
+    <h4>Months</h4>
+  {
+    store.months.map((month) => <div><CustomLink to={"/stats/" + month}>{month}</CustomLink></div>)
+  }
+  </div>
+
+  <div>
+  <h4>Weeks</h4>
+  {
+    store.weeks.map((week) => <div><CustomLink to={"/stats/" + week}>{week}</CustomLink></div>)
+  }
+  </div>
 </section>
 {
   /*have a bunch of links to:
