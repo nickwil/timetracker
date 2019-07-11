a087f8e7965671a0c0a39185a5abc86910984174
Nicolas <nickwil@users.noreply.github.com>
2019-07-11 10:43:36 -0400

    outline current task being worked on

--- src/main/item/ItemList.jsx
+++ src/main/item/ItemList.jsx
@@ -4,15 +4,23 @@ import Input from "../../general/Input.jsx";
 import ItemTime from "./ItemTime.jsx";
 import ItemMenuContainer from "./ItemMenu.jsx";
 import { observer } from "mobx-react-lite";
+import {timeStore} from "../../stores/store.js"
 import tagStore from "../../stores/tagStore.js";
 import shortTimeFormatting from "../../util/shortTimeFormatting.js";
 
 import styles from "./ItemList.module.css";
 
 const ItemList = observer(function ItemList({ store, item, tags }) {
+  const checkItem = () => {
+    if(timeStore.selectedItem == item.id){
+      return styles.container + " " + styles.outline
+    } else {
+      return styles.container
+    }
+  }
   const storeItem = store.items[store.index(item.id)];
   return (
-    <div className={styles.container}>
+    <div className={checkItem()}>
       { item.completed?
         shortTimeFormatting(item.length)
         :
@@ -41,4 +49,5 @@ const ItemList = observer(function ItemList({ store, item, tags }) {
     </div>
   );
 });
+
 export default ItemList;
