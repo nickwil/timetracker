e1011ad757293e88129dfdb84fad6909347a58c3
Nicolas <nickwil@users.noreply.github.com>
2019-07-15 13:18:51 -0400

    added tests to util folder

--- src/util/shortTimeFormatting.js
+++ src/util/shortTimeFormatting.js
@@ -4,8 +4,6 @@ function shortTimeFormatting(time){
 	const minutes = "m"
 	const hours = "h"
 
-	var newTime = time
-	console.log("seconds")
 	
 	if(time <= 60){
 		return time + seconds
@@ -14,11 +12,9 @@ function shortTimeFormatting(time){
 		return Math.round((time / 60) * 10) / 10 + minutes
 	} else if (time >= 3600){
 		return Math.round((time / 3600) * 10) / 10 + hours
-	} else {
-		return newTime = time + seconds
 	}
 
-	return newTime
+	return time
 	
 	
 }
