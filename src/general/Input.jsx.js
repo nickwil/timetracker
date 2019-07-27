24d8034d18f65d49754b0c9043ac6af940083445
Nicolas <nickwil@users.noreply.github.com>
2019-07-27 16:29:16 -0400

    changed Input component to not rely on it's own internal version of the text prop to ensure that it would always be up to date and remove any need for a componentDidUpdate method

--- src/general/Input.jsx.js
+++ src/general/Input.jsx.js
@@ -1,18 +1,14 @@
 import React from "react";
 class Input extends React.Component {
-  state = { text: this.props.text };
-  onChange(e) {
-    this.setState({ text: e.target.value });
-    this.props.onTaskChange(e.target.value);
-  }
+
   render() {
     return (
       <input
       {...this.props}
         className={this.props.styles}
         style={this.props.style}
-        onChange={e => this.onChange(e)}
-        value={this.state.text}
+        onChange={e => this.props.onTaskChange(e.target.value)}
+        value={this.props.text}
       />
     );
   }
