ec9f64ea5134f2f5f9333e2e7e33a41176f1da30
Nicolas <nickwil@users.noreply.github.com>
2019-07-27 20:21:59 -0400

    simplified Tags component, elimated the Tag component and removed an error by changing from using the selected prop on an option element to the defaultValue prop on the select element

--- src/main/Tags.jsx
+++ src/main/Tags.jsx
@@ -2,24 +2,13 @@ import React from "react";
 import { observer } from "mobx-react-lite";
 
 const Tags = observer(function Tags({ tags, onChange, defaultTagId }) {
+  const defaultTag = tags.filter(tag => tag.id === defaultTagId)[0].id
   return (
-    <select data-testid="tags-selection" onChange={e => onChange(e.target.value)}>
+    <select defaultValue={defaultTag} data-testid="tags-selection" onChange={e => onChange(e.target.value)}>
       {tags.map((tag, index) => (
-        <Tag key={"tag-key-id"+tag.id} tag={tag} defaultTagId={defaultTagId} />
+        <option key={"tag-key-id"+tag.id} value={tag.id}>{tag.name}</option>
       ))}
     </select>
   );
-});
-
-const Tag = observer(function Tag({ tag, defaultTagId }) {
-  if (defaultTagId === tag.id) {
-    return (
-      <option selected="selected" value={tag.id}>
-        {tag.name}
-      </option>
-    );
-  } else {
-    return <option value={tag.id}>{tag.name}</option>;
-  }
 });
 export default Tags;
