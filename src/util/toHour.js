a3447edc19f28a8140566ccd6b4c6863d791d080
Nicolas <nickwil@users.noreply.github.com>
2019-07-28 12:50:33 -0400

    changed toHour function so that way the timer can show timers greater than an hour

--- src/util/toHour.js
+++ src/util/toHour.js
@@ -4,14 +4,16 @@ function toHour(time) {
   var minutes = Math.floor((sec_num - hours * 3600) / 60);
   var seconds = sec_num - hours * 3600 - minutes * 60;
 
-  // if (hours   < 10) {hours   = "0"+hours;}
+  if (hours   < 10) {hours   = "0"+hours;}
   if (minutes < 10) {
     minutes = "0" + minutes;
   }
   if (seconds < 10) {
     seconds = "0" + seconds;
   }
-  //return hours+':'+minutes+':'+seconds;
+  if(hours > 0){
+    return hours+':'+minutes+':'+seconds;
+  }
   return minutes + ":" + seconds;
 }
 export default toHour
